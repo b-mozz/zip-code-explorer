0b5f582a28c0ca0b997df3cad1a2650f812eb29c
b-mozz <165301979+b-mozz@users.noreply.github.com>
2025-01-04 03:17:25 -0500

    Delete src/tempCodeRunnerFile.py

--- src/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-    income_data = read_csv_file(
-        "data/uszips.csv",
-        skiprows=2583,
-        nrows=1825,
-        column_names=pd.read_csv("data/uszips.csv", nrows=1).columns
-    )
-    crime_data = read_csv_file("data/arrestData.csv")
-    home_value = read_csv_file("data/average_value.csv")
